380246eb18a0484d6858635e43e9c8065fe2715f
Sarvesh Khire <sarvesh.khire@snowflake.com>
2021-02-23 20:09:08 +0530

    changes entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,11 @@ format_sql=.sql
 
 echo $2
 echo $3
+python_changed_files=()
 
 git fetch origin $2:$2
 git fetch origin $3:$3
-MODIFIED_FILES_python="$(git diff --name-only $2 $3 | grep -E "(${format_py})")"
+MODIFIED_FILES_python=$(git diff --name-only $2 $3 | grep -E "(${format_py})")
 MODIFIED_FILES_sql=$(git diff --name-only $2 $3 | grep -E "(${format_sql})")
 
 # head=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
@@ -21,9 +22,10 @@ MODIFIED_FILES_sql=$(git diff --name-only $2 $3 | grep -E "(${format_sql})")
 # MODIFIED_FILES_sql=$(git diff --name-only $head main | grep -E "(${format_sql})")
 
 echo "Modified python files1: "$MODIFIED_FILES_python
-echo "Modified python files2: "$MODIFIED_FILES_python
-echo "Modified python files3: "$MODIFIED_FILES_python
 echo "Modified sql files: "$MODIFIED_FILES_sql
 
-echo "::set-output name=python_changed_files::${MODIFIED_FILES_python}"
+python_changed_files+=("${MODIFIED_FILES_python}")
+
+
+echo "::set-output name=python_changed_files::${python_changed_files}"
 echo "::set-output name=sql_changed_files::${MODIFIED_FILES_sql}"
