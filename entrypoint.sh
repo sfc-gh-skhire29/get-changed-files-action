f6cfa5773690902ab98a3abc34bfb683bd8cceae
Sarvesh Khire <sarvesh.khire@snowflake.com>
2021-02-23 17:42:47 +0530

    changes entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,17 @@
 
 format_py=.py
 format_sql=.sql
-MODIFIED_FILES_python=$(git diff --name-only $2 $3 | grep -E "(${format_py})")
-MODIFIED_FILES_sql=$(git diff --name-only $3 $3 | grep -E "(${format_sql})")
+
+echo $2
+echo $2
+
+#MODIFIED_FILES_python=$(git diff --name-only $2 $3 | grep -E "(${format_py})")
+#MODIFIED_FILES_sql=$(git diff --name-only $3 $3 | grep -E "(${format_sql})")
+
+head=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+MODIFIED_FILES_python=$(git diff --name-only $head main | grep -E "(${format_py})")
+MODIFIED_FILES_sql=$(git diff --name-only $head main | grep -E "(${format_sql})")
+
 
 echo "Modified python files: "$MODIFIED_FILES_python
 echo "Modified sql files: "$MODIFIED_FILES_sql
