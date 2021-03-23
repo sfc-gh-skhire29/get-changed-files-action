8bbe70e3d7b0343d29fb75b25b8f3ea6cb6a1557
Sarvesh Khire <sarvesh.khire@snowflake.com>
2021-03-23 20:43:12 +0530

    support for .txt files

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@
 
 format_py=.py
 format_sql=.sql
+format_txt=.txt
 
 echo $2
 echo $3
@@ -17,14 +18,15 @@ git fetch origin $3:$3
 
 MODIFIED_FILES_python=$(git diff --name-only $2 $(git merge-base $2 $3) | grep -E "(${format_py})")
 MODIFIED_FILES_sql=$(git diff --name-only $2 $(git merge-base $2 $3) | grep -E "(${format_sql})")
-
+MODIFIED_FILES_txt=$(git diff --name-only $2 $(git merge-base $2 $3) | grep -E "(${format_txt})")
 # head=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 # MODIFIED_FILES_python=$(git diff --name-only $head main | grep -E "(${format_py})")
 # MODIFIED_FILES_sql=$(git diff --name-only $head main | grep -E "(${format_sql})")
 
 echo "Modified python files1: "$MODIFIED_FILES_python
 echo "Modified sql files: "$MODIFIED_FILES_sql
-
+echo "Modified sql files: "$MODIFIED_FILES_txt
 
 echo "::set-output name=python_changed_files::$(echo $MODIFIED_FILES_python)"
 echo "::set-output name=sql_changed_files::$(echo $MODIFIED_FILES_sql)"
+echo "::set-output name=txt_changed_files::$(echo $MODIFIED_FILES_txt)"
